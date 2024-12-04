299cea5407ca537ef43c459cbf5d5516bd44b7cd
rajparmarr2308 <rajparmar23801@gmail.com>
2024-12-04 12:30:35 +0530

    Added Models for OpenAI, Anthropic, Gemini. Refactor model pricing structure

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="agent-guard",
-    version="0.1.2",
+    version="0.1.3",
     author="Raj Parmar",
     author_email="rajparmar2308@gmail.com",
     description="An operational monitoring library for Crew AI applications.",
